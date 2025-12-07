02931cf014fd160b9dba641fbdf9d9bcc4323a78
destiny326 <destinyrma4@gmail.com>
2025-12-06 21:46:03 -1200

    Update config.php

--- config.php
+++ config.php
@@ -5,12 +5,15 @@ define('DB_SERVER', 'mysql-10dead2a-crudd.g.aivencloud.com');
 define('DB_USERNAME', 'avnadmin');
 define('DB_PASSWORD', '$2y$10$hPvLTgiV86nYkTbsg4yV/ep1E6VlfspZkf6jY9i4ybiEFDGsO2Tq.');
 define('DB_NAME', 'cruddapp');
+define('DB_PORT', '17808');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
+
+
